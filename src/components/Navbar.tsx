c7c6201d61d06ff063c93cd014868a2eadfc1f3f
DreMukare <dremukare@gmail.com>
2024-01-29 17:31:00 +0300

    feat: built navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import { useState } from 'react';
+import './Navbar.sass';
 
 const Navbar = () => {
+	const [location, setLocation] = useState('Home');
 	const navLinkTitles = ['Home', 'Profile', 'Contact'];
 
 	const NavLink = (props: { title: string }) => {
 		const { title } = props;
-		return <>{title}</>;
+		return (
+			<button
+				onClick={() => {
+					setLocation(title);
+				}}
+				className="navLink"
+				style={{
+					backgroundColor: location === title ? '#fff' : 'inherit',
+				}}
+			>
+				{title}
+			</button>
+		);
 	};
 
 	return (
-		<div>
+		<div className="navContainer">
 			{navLinkTitles.map((title: string, idx: number) => (
 				<NavLink key={idx} title={title} />
 			))}
