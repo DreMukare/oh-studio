c06c708ac9122ea5f9e61a6e555ac973575ee4bc
DreMukare <dremukare@gmail.com>
2024-01-29 17:45:48 +0300

    feat: built out footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import './Footer.sass';
 
 const Footer = () => {
-	return <div>Footer</div>;
+	const socialMediaLinkTitles = ['Facebook', 'Twitter', 'LinkedIn'];
+
+	return (
+		<div className="footerContainer">
+			<div>Copyright 2024</div>
+			<div className="footerLinkContainer">
+				{socialMediaLinkTitles.map((title: string, idx: number) => (
+					<a
+						key={idx}
+						href={`https://${title.toLowerCase()}.com`}
+						className="footerLink"
+					>
+						{title}
+					</a>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
