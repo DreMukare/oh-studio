881a4760bc64e7413c8a29d0b11b5425be8125c8
DreMukare <dremukare@gmail.com>
2024-02-15 14:58:25 +0300

    minor: added props to headertext component

--- src/components/HeaderText.tsx
+++ src/components/HeaderText.tsx
@@ -1,8 +1,16 @@
 import './HeaderText.sass';
 
-const HeaderText = (props: { text: string }) => {
-	const { text } = props;
-	return <p className="headerText">{text}</p>;
+const HeaderText = (props: {
+	text: string;
+	width: string;
+	style: { [key: string]: string };
+}) => {
+	const { text, width, style } = props;
+	return (
+		<p className="headerText" style={{ width: width, ...style }}>
+			{text}
+		</p>
+	);
 };
 
 export default HeaderText;
