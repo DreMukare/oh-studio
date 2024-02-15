d935f4853669e0b5b9269d067eb7f619a99d5f02
DreMukare <dremukare@gmail.com>
2024-02-15 14:58:07 +0300

    minor: updated footer content and styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,33 @@
+import { Link, useLocation } from 'react-router-dom';
 import './Footer.sass';
 
 const Footer = () => {
 	const socialMediaLinkTitles = ['Facebook', 'Twitter', 'LinkedIn'];
+	const location = useLocation();
 
 	return (
 		<div className="footerContainer">
-			<div>Copyright 2024</div>
-			<div className="footerLinkContainer">
-				{socialMediaLinkTitles.map((title: string, idx: number) => (
-					<a
-						key={idx}
-						href={`https://${title.toLowerCase()}.com`}
-						className="footerLink"
-					>
-						{title}
-					</a>
-				))}
+			{location.pathname !== '/contact' && (
+				<div className="callToAction">
+					<h2>Let's work together</h2>
+					<Link to="/contact" style={{ textDecoration: 'none' }}>
+						<h2 className="h2Link">Get in touch.</h2>
+					</Link>
+				</div>
+			)}
+			<div className="linksAndCopyright">
+				<div>Copyright 2024</div>
+				<div className="footerLinkContainer">
+					{socialMediaLinkTitles.map((title: string, idx: number) => (
+						<a
+							key={idx}
+							href={`https://${title.toLowerCase()}.com`}
+							className="footerLink"
+						>
+							{title}
+						</a>
+					))}
+				</div>
 			</div>
 		</div>
 	);
