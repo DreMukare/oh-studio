f03c7892524efb7cebbcc90c578f269640e3269b
DreMukare <dremukare@gmail.com>
2024-02-20 15:01:26 +0300

    minor: completed marketingtext component

--- src/components/MarketingText.tsx
+++ src/components/MarketingText.tsx
@@ -2,9 +2,27 @@ import MarketingTag from './MarketingTag';
 
 const MarketingText = ({ title, text }: { title: string; text: string }) => {
 	return (
-		<div>
+		<div
+			style={{
+				width: '50%',
+				margin: 'auto',
+				fontFamily: 'Poppins, sans-serif',
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}
+		>
 			<MarketingTag title={title} />
-			<p className="marketingText">{text}</p>
+			<p
+				style={{
+					fontWeight: 'bold',
+					fontSize: '2em',
+					textAlign: 'center',
+				}}
+			>
+				{text}
+			</p>
 		</div>
 	);
 };
